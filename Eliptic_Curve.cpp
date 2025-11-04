00189abb1827a5dbc1b3f2d638e53f42cddc14fd
amrutadahatonde10 <amrutadahatonde10@gmail.com>
2025-11-04 09:17:41 +0530

    04-11-2025

--- Eliptic_Curve.cpp
+++ Eliptic_Curve.cpp
@@ -2,29 +2,25 @@
 
 ECPoint ELCurve::pointAdd(const ECPoint &P, const ECPoint &Q) const
 {
-    if (P.infinity)
-        return Q;
-    if (Q.infinity)
-        return P;
+    if (P.infinity) return Q;
+    if (Q.infinity) return P;
 
+    // Point at infinity if P == -Q
     if (P.x == Q.x && P.y == -Q.y)
         return ECPoint();
 
     ZZ_p lambda;
     if (P.x != Q.x)
-    {
         lambda = (Q.y - P.y) / (Q.x - P.x);
-    }
     else
-    {
-        ZZ_p lambda = (3 * sqr(P.x) + a) / (2 * P.y);
-    }
+        lambda = (3 * sqr(P.x) + a) / (2 * P.y);  
 
     ZZ_p x3 = sqr(lambda) - P.x - Q.x;
     ZZ_p y3 = lambda * (P.x - x3) - P.y;
 
     return ECPoint(x3, y3);
 }
+
 
 ECPoint ELCurve::pointDouble(const ECPoint &P) const
 {
